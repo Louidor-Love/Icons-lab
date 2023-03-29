1ea6d929bd5c7a83efce976d3dfb3a0ce12bcc47
Louidor-Love <louidorlove@gmail.com>
2023-03-29 02:31:35 -0300

    inicial commit

---
+++ src/test/java/com/Alk/icons/icons/IconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alk.icons.icons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
